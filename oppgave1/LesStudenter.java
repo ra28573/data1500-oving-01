c5208c520b4568fc01a6f01e79cf5a7af8f3a97c
Janis Gailis <janis.gailis@uia.no>
2026-01-06 10:05:04 +0100

    grunnkode

--- oppgave1/LesStudenter.java
+++ oppgave1/LesStudenter.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-// 1. Definere en klasse for data om studenter (Student)
+// 1. Definere en klasse for data fra en datafil om studenter basert på strukturen i data (se oppgavetekst i README.md)
 class Student {
+    // Skriv kode her
 
-    
+    // Metoden erstatter en eksisterende metode toString() 
     @Override
     public String toString() {
         return "Student{" + .... + "}";
@@ -15,12 +16,13 @@ class Student {
 }
 
 // 2. Bruk try-with-resources for sikker filbehandling og les inn data fra fil i en Java datastruktur 
-// ved bruk av Scanner, linje for linje
+// ved bruk av Scanner, linje for linje, og til slutt skriv ut datafeltene til System.out
 public class LesStudenter {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(new File("test.txt"))) {
+        try (Scanner scanner = new Scanner(new File(args[0]))) {
             while (scanner.hasNext()) {
-                // Bruk scanner.nextLine() for allokere Student-objekter for hver linje i students.csv
+                // Bruk scanner.nextLine() for allokere Student-objekt for hver linje i students.csv
+                // og skriv ut dataene med den overskrevede toString() metoding i klassen Student
                 // ...
             }
         } catch (FileNotFoundException fnfe) {
